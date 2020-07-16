eb18e826ffc735726f0584202f497ea539cd8dc0
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-16 03:24:09 +0300

    delta

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Router from "next/router";
 
 export default function App({ Component, pageProps }) {
   Router.events.on("routeChangeComplete", () => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV !== "production") {
       const els = document.querySelectorAll(
         'link[href*="/_next/static/css/styles.chunk.css"]',
       );
